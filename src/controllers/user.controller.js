add9be12e5a68c0d5fb0309dbd14b61a45d91181
Plycedes <abhaypratapyadav340@gmail.com>
2025-01-05 15:52:19 +0530

    Register User

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -22,7 +22,41 @@ const generateAccessAndRefreshTokens = async (userId) => {
 };
 
 export const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "OK",
+  //   res.status(200).json({
+  //     message: "OK",
+  //   });
+
+  const { email, username, password } = req.body;
+
+  // console.log(`Data ${email} ${username} ${password}`);
+
+  if ([email, username, password].some((field) => field?.trim() === "")) {
+    throw new ApiError(400, "No field can be empty");
+  }
+
+  const existingUser = await User.findOne({
+    $or: [{ username }, { email }],
   });
+
+  if (existingUser) {
+    throw new ApiError(409, "Username or Email already exists");
+  }
+
+  const user = await User.create({
+    username: username.toLowerCase(),
+    email,
+    password,
+  });
+
+  const userCreated = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!userCreated) {
+    throw new ApiError(500, "Something went wrong while creating the user");
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, userCreated, "User registered successfully"));
 });
